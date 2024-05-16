b3544abe8f8d15d1fdad223113c8038113bfdd6e
GODWIN <e-godwinbwambale@outlook.com>
2024-05-16 09:59:25 +0300

    Console APP

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello Dady This Is your boy Ezra Bwambale Godwin");
+int x;
+int y;
+
+x = 70;
+y = x + 30;
+
+Console.WriteLine(y+50);
