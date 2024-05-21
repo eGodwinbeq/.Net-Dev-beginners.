975500b37d05efc22de93b7512bb84ce885cec97
GODWIN <e-godwinbwambale@outlook.com>
2024-05-21 22:08:53 +0300

    Consol app game/ beginning logic

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
 Console.WriteLine("Hello Dady This Is your boy Ezra Bwambale Godwin");
+ * 
 int x;
 int y;
 
 x = 70;
 y = x + 30;
-
 Console.WriteLine(y+50);
+
+*/
+
+// lets do some logic here
+Console.WriteLine("To days wheel of luck");
+Console.Write("Enter you lucky Numbers");
+
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3544abe8f8d15d1fdad223113c8038113bfdd6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
